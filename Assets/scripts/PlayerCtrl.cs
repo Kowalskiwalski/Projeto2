f12271516acae205edd19141441ed5d38a0ef8b2
Lucas Ferreira <lucasfb159@gmail.com>
2018-06-25 12:05:17 -0300

    Controlando as animacoes

--- Assets/scripts/PlayerCtrl.cs
+++ Assets/scripts/PlayerCtrl.cs
@@ -12,10 +12,15 @@ public class PlayerCtrl : MonoBehaviour {
 
 	SpriteRenderer sr;
 
+	Animator anim;
+
+	bool isjumping = false;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr= GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -33,25 +38,46 @@ public class PlayerCtrl : MonoBehaviour {
 		if(Input.GetButtonDown("Jump")){
 			Jump();
 		}
+
+		ShowFalling();
 	}
 
 	void MoveHorizontal(float speed) {
 		rb.velocity = new Vector2(speed, rb.velocity.y);
 
 		if(speed < 0f) {
-			sr.flipX = true;
+			sr.flipX = false;
 		}
 		else if (speed > 0f) {
-			sr.flipX = false;
+			sr.flipX = true;
+		}
+
+		if (!isjumping){
+			anim.SetInteger("State", 2); 
 		}
 	}
 
 	void StopMovingHorizontal(){
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+		if (!isjumping){
+		anim.SetInteger("State", 0);
+		}
+	}
 
+	void ShowFalling(){
+		if(rb.velocity.y < 0f){
+			anim.SetInteger("State", 3);
+		}
 	}
 
 	void Jump(){
+		isjumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Ground")){
+			isjumping = false; 
+		}
 	}
 }
