a052abff11ebfa7a81d710b61d5ab088ccfac02a
Lucas Ferreira <lucasfb159@gmail.com>
2018-08-17 10:36:29 -0300

    Incrementando Timer e contador de moedas

--- Assets/scripts/GM.cs
+++ Assets/scripts/GM.cs
@@ -16,6 +16,12 @@ public class GM : MonoBehaviour {
 
 	public  float  timeToRespawn = 2f;
 
+	public float maxTime = 120f;
+
+	bool timerOn = true;
+
+	float timeLeft;
+
 	public UI ui;
 
 	GameData data = new GameData();
@@ -30,7 +36,8 @@ public class GM : MonoBehaviour {
 		if (player == null){
 			RespawnPlayer();
 		}
-		
+
+		timeLeft = maxTime;
 	}
 	
 	void Update () {
@@ -40,15 +47,27 @@ public class GM : MonoBehaviour {
 				player = obj.GetComponent<PlayerCtrl>();
 			}
 		}
+		UpdateTimer();
 		DisplayHudData();
+	}
+
+	void UpdateTimer(){
+		if(timerOn){
+			timeLeft = timeLeft - Time.deltaTime;
+			if (timeLeft <= 0){
+				timeLeft = 0;
+				ExpirePlayer();
+			}
+		}
 	}
 
 	void DisplayHudData(){
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtTimer.text = timeLeft.ToString("F1");
 	}
 
 	public void IncrementCoinCount(){
-		data.coinCount++;
+		data.coinCount ++;
 	}
 
 	public void RespawnPlayer(){
@@ -60,5 +79,16 @@ public class GM : MonoBehaviour {
 			Destroy(player.gameObject);
 			Invoke("RespawnPlayer", timeToRespawn);
 		}
+	}
+	
+	public void ExpirePlayer(){
+		if (player != null) {
+			Destroy(player.gameObject);
+		}
+		GameOver();
+	}
+	void GameOver(){
+		timerOn = false;
+		ui.gameOver.gameOverPanel.SetActive(true);
 	}
 }
